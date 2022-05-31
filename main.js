610c2f4cc3f7bdacab908502eecc456d523ad56a
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-05-31 14:01:46 -0400

    filtrado de las imagenes

--- main.js
+++ main.js
@@ -10,4 +10,17 @@ window.addEventListener('load', () => {
     grid.refreshItems().layout();
 
     document.getElementById('grid').classList.add('imagenes-cargadas');
-})
+
+    //Parte del filtrado de las imagenes, en la constante enlaces se estan guardando todas las categorias del header
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener('click', (evento) => {
+            evento.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+            evento.target.classList.add('activo');
+
+            const categoria = evento.target.innerHTML.toLowerCase();
+            categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+        });
+    });
+});
